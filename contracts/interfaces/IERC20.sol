2c51de53a137d2ba9ba6eb2b98f475e7b6730d6c
SouparnoGhosh <ghosh99souparno@gmail.com>
2022-05-14 16:30:53 +0530

    Added interface IERC20

---
+++ contracts/interfaces/IERC20.sol
@@ -0,0 +1,36 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.4;
+
+interface IERC20 {
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function name() external view returns (string memory);
+
+    function symbol() external view returns (string memory);
+
+    function decimals() external view returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address _owner) external view returns (uint256);
+
+    function allowance(address _owner, address _spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address _spender, uint256 _value) external returns (bool);
+
+    function transfer(address _to, uint256 _value) external returns (bool);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) external returns (bool);
+}
